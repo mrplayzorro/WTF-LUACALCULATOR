710a5e7c9fd90b45e9e44cc11997576bb61518f8
mrplayzorro <102983952+mrplayzorro@users.noreply.github.com>
2022-09-13 18:00:13 +0200

    Update main that i didnt finish lmaooo.lua
    lmaoo this is not like some advanced shit so don't put 300 fucking dimensions + no gui cuz im working on it lol

--- main that i didnt finish lmaooo.lua
+++ main that i didnt finish lmaooo.lua
@@ -1,1 +1,7 @@
-
+--[[For now you take this cuz fuck you, execute it or idk]]
+--for executors lmao
+local shit = --put your shit here (*, /, +, - and numbers of course)
+setclipboard("shit")
+--for lua demo 
+local monkey = --put your shit here (*, /, +, - and numbers of course)
+print("monkey")
